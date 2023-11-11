f7d8f025462f78df8cb8c5cbb5e93e90b963d923
Elena Cristescu <elenacristescu11@gmail.com>
2023-11-11 19:42:19 +0100

    update

--- script.js
+++ script.js
@@ -460,8 +460,9 @@ const calcDaysPassed = (date1, date2) =>
 
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
 console.log(days1);
-*/
+
 const num = 236492863.22;
 console.log('US:     ', new Intl.NumberFormat('en-US').format(num));
 console.log('Germany:', new Intl.NumberFormat('de-DE').format(num));
 console.log('Syria:', new Intl.NumberFormat('ar-SY').format(num));
+*/
